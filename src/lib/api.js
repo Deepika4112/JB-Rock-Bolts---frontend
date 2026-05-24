b64fb006ae13139b106f800684c68fec1a0f36a2
Deepika <deepikar412003@gmail.com>
2026-05-24 16:14:27 +0530

    last update

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,6 @@
-const BASE = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const BASE =
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:8000";
 
 const getToken = () => localStorage.getItem("auth_token");
 
